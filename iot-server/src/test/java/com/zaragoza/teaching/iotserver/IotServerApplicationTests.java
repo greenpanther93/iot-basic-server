225c4c73b709b023f52fb865b74c4a108a43eff9
pascalzaragoza <pascal.zaragoza@yahoo.com>
2019-08-08 23:06:49 +0200

    adding initial springboot / maven project files

---
+++ iot-server/src/test/java/com/zaragoza/teaching/iotserver/IotServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zaragoza.teaching.iotserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IotServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
