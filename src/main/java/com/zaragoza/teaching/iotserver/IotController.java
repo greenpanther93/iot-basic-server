62a01fab3482b6e50697096b3bea7beaa98eaf36
pascalzaragoza <pascal.zaragoza@yahoo.com>
2019-08-09 00:26:13 +0200

    adding error page

--- src/main/java/com/zaragoza/teaching/iotserver/IotController.java
+++ src/main/java/com/zaragoza/teaching/iotserver/IotController.java
@@ -24,4 +24,10 @@ public class IotController {
 	    
 	    return messages;
 	  }
+	  
+	  @GetMapping("/error")
+	  public String error(){
+	    
+	    return "This is the error page, for when nothing is working.";
+	  }
 }
