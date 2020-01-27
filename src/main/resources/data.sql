2713ce07e0912951faee0b098c45af23f2bf209e
greenpanther93 <pascal.zaragoza@yahoo.com>
2020-01-27 15:51:06 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,34 +1,35 @@
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,49.74,2.38,'Bonjour de Somme !');
+values(20130039,45.746496,3.194542,'Dev Web recherche travail sympa. Please help');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,48.065687,-0.3355233,'ici c est Pays de la Loire');
+values(20132226,49.74,2.38,'Bonjour de Somme !');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,48.939187,2.301485,'Salut de Paris');
+values(20133242,48.065687,-0.3355233,'ici c est Pays de la Loire');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,43.757701,1.422579,'Toulouse');
+values(20136149,48.939187,2.301485,'Salut de Paris');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,43.527141,7.003634,'Cannes le festival');
+values(20134299,43.757701,1.422579,'Toulouse');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,43.598789,3.872530,'Montpellier');
+values(20139956,43.527141,7.003634,'Cannes le festival');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.762250,4.828341,'Je suis ici');
+values(20136724,43.598789,3.872530,'Montpellier');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.846496,3.0594542,'Moi ici !!');
+values(20136575,45.762250,4.828341,'Je suis ici');
+insert into message(student_id,gps_lat,gps_long,student_message)
+values(20132159,45.846496,3.0594542,'Moi ici !!');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,45.846496,3.600002,'Festival du film de clermont !');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.846476,3.100000,'Les films sont bien ici');
+values(20132159,45.846476,3.100000,'Les films sont bien ici');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.846486,3.884543,'coucou');
+values(20136724,45.846486,3.884543,'coucou');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.146496,3.1582542,'salut !');
+values(20139956,45.146496,3.1582542,'salut !');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.866496,3.8094542,'Hallo');
+values(20134299,45.866496,3.8094542,'Hallo');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,46.000,3.1194542,'Coucou de France');
+values(20136149,46.000,3.1194542,'Coucou de France');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.946496,3.6594542,'Message test..');
+values(20133242,45.946496,3.6594542,'Message test..');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.346496,3.3594542,'Quelqu un peut m aider à trouver mon portable ?');
-insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.746496,3.194542,'Dev Web recherche travail sympa. Please help');
+values(20132226,45.346496,3.3594542,'Quelqu un peut m aider à trouver mon portable ?');
+
