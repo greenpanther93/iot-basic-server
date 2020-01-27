b1fa9876fddf5fdc3a7fa9827c4d4ec448e4ceee
nhlad <nicolas.hlad@etu.umontpellier.fr>
2020-01-27 15:39:32 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,49.74,2.38,'Bonjour de Somme !');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,48.065687,-0.3355233,'ici c'est Pays de la Loire');
+values(20130039,48.065687,-0.3355233,'ici c est Pays de la Loire');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,48.939187,2.301485,'Salut de Paris');
 insert into message(student_id,gps_lat,gps_long,student_message)
@@ -11,7 +11,7 @@ values(20130039,43.527141,7.003634,'Cannes le festival');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,43.598789,3.872530,'Montpellier');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.762250,4.828341,'Je suis là');
+values(20130039,45.762250,4.828341,'Je suis ici');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,45.846496,3.0594542,'Moi ici !!');
 insert into message(student_id,gps_lat,gps_long,student_message)
@@ -29,6 +29,6 @@ values(20130039,46.000,3.1194542,'Coucou de France');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,45.946496,3.6594542,'Message test..');
 insert into message(student_id,gps_lat,gps_long,student_message)
-values(20130039,45.346496,3.3594542,'Quelqu'un peut m'aider à trouver mon portable ?');
+values(20130039,45.346496,3.3594542,'Quelqu un peut m aider à trouver mon portable ?');
 insert into message(student_id,gps_lat,gps_long,student_message)
 values(20130039,45.746496,3.194542,'Dev Web recherche travail sympa. Please help');
